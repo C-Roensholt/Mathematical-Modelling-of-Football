bf84b4ce0ed3cc29c646ae363aa4221ca196f548
Christian Rønsholt <ronsholt32@gmail.com>
2022-09-03 18:48:22 +0200

    reduced to two figures

--- 1. Hand-in/pedri_euro_2020.py
+++ 1. Hand-in/pedri_euro_2020.py
@@ -90,91 +90,55 @@ mask_penalty_area = ((df_player_passes.end_x >= 100) &
 df_penalty_area = df_player_passes.loc[mask_penalty_area]
 
 ## --------- PLOT PLAYER PASSES --------- ##
-pitch = Pitch(line_color="black")
-vert_pitch = VerticalPitch(line_color="black", half=True)
-
-layout = [["A", "B", "E", "F"],
-          ["C", "D", "E", "F"]]
-fig, axes = plt.subplot_mosaic(layout, constrained_layout=True, figsize=(20,12),
-                               gridspec_kw={"width_ratios": [2, 2, 1, 0.2]})
-
-## -------- PLOT FINAL THIRD AND INTO PENALTY BOX PASSES (PITCH A)-------- ##
-vert_pitch.draw(ax=axes["A"])
-# plot final third
-vert_pitch.lines(df_final_third.x, df_final_third.y,
-                 df_final_third.end_x, df_final_third.end_y, comet=True,
-                 ax=axes["A"], lw=6, color="b", alpha=0.4, zorder=5)
-vert_pitch.scatter(df_final_third.end_x, df_final_third.end_y, ax=axes["A"],
-                   s=200, edgecolor="w", lw=3, c="b", alpha=1, zorder=5)
-axes["A"].axhline(80, 0, 80, lw=3, ls=":", color="k", zorder=10)
-# plot into penalty area passes
-vert_pitch.lines(df_penalty_area.x, df_penalty_area.y,
-                 df_penalty_area.end_x, df_penalty_area.end_y, comet=True,
-                 ax=axes["A"], lw=6, color="r", alpha=0.4, zorder=5)
-vert_pitch.scatter(df_penalty_area.end_x, df_penalty_area.end_y, ax=axes["A"],
-                   s=200, edgecolor="w", lw=3, c="r", alpha=1, zorder=5)
-
-## -------- PLOT DEEP COMPLETIONS (PITCH B)-------- ##
-vert_pitch.draw(ax=axes["B"])
-circle = patches.Circle([40,120],radius = radius , linewidth = 1.5, linestyle='--',
-                        edgecolor='#070707', facecolor='none', alpha=1, zorder=3 )
-axes["B"].add_patch(circle)
-# plot progressive passes
-vert_pitch.lines(df_prog_passes.x, df_prog_passes.y,
-                 df_prog_passes.end_x, df_prog_passes.end_y, comet=True,
-                 ax=axes["B"], lw=6, color="r", alpha=0.4, zorder=5)
-vert_pitch.scatter(df_prog_passes.end_x, df_prog_passes.end_y, ax=axes["B"],
-                   s=200, edgecolor="w", lw=3, c="r", alpha=1, zorder=5)
-# plot deep completions
-vert_pitch.lines(df_deep_completions.x, df_deep_completions.y,
-                 df_deep_completions.end_x, df_deep_completions.end_y, comet=True,
-                 ax=axes["B"], lw=6, color="b", alpha=0.4, zorder=5)
-vert_pitch.scatter(df_deep_completions.end_x, df_deep_completions.end_y, ax=axes["B"],
-                   s=200, edgecolor="w", lw=3, c="b", alpha=1, zorder=5)
-
-## -------- PLOT SWITCHES (PITCH C) ----------- ##
-# plot passes
-# vert_pitch.draw(ax=axes["C"])
-# vert_pitch.lines(df_player_passes.x, df_player_passes.y,
-#             df_player_passes.end_x, df_player_passes.end_y, comet=True,
-#             ax=axes["C"], lw=6, color="k", alpha=0.2)
-# vert_pitch.scatter(df_player_passes.end_x, df_player_passes.end_y, ax=axes["C"],
-#               s=200, edgecolor="w", lw=3, color="k", alpha=0.1, zorder=5)
-# plot switches
-vert_pitch.draw(ax=axes["C"])
-vert_pitch.lines(df_switches.x, df_switches.y,
-            df_switches.end_x, df_switches.end_y, comet=True,
-            ax=axes["C"], lw=6, color="b", alpha=0.2)
-vert_pitch.scatter(df_switches.end_x, df_switches.end_y, ax=axes["C"],
-              s=200, edgecolor="w", lw=3, color="b", alpha=1, zorder=10)
-
-## -------- PLOT PROGRESSIVE PASSES (PITCH D)-------- ##
-vert_pitch.draw(ax=axes["D"])
-
-
-## ------- PLOT PERCENTILE RANKING (E) ----------- ##
+# create colormap
 soc_cm = mcolors.LinearSegmentedColormap.from_list('SOC', gradient, N=50)
 cm.register_cmap(name='SOC', cmap=soc_cm)
-
-# get player stats for percentile plot
 norm = mcolors.Normalize(vmin=0,
                          vmax=1)
 cmap = plt.get_cmap('SOC')
-bars = axes["E"].barh(y=df_fbref_percentile_player.index,
+
+# draw pitch
+pitch = Pitch(line_color="black")
+vert_pitch = VerticalPitch(line_color="black", half=True)
+
+layout = [["A", "B", "C"]]
+fig, axes = plt.subplot_mosaic(layout, constrained_layout=True, figsize=(20,12),
+                               gridspec_kw={"width_ratios": [2, 1, 0.2]})
+
+## -------- PLOT FINAL THIRD AND INTO PENALTY BOX PASSES (PITCH A)-------- ##
+vert_pitch.draw(ax=axes["A"])
+# get the 2D histogram
+bin_statistic = vert_pitch.bin_statistic(df_prog_passes.x, df_prog_passes.y,
+                                         statistic="count", bins=(6, 5), normalize=True)
+# make a heatmap
+pcm  = vert_pitch.heatmap(bin_statistic, cmap=cmap, edgecolor="k",
+                          zorder=-2, ax=axes["A"], lw=3, ls="--")
+vert_pitch.arrows(df_prog_passes.x, df_prog_passes.y,
+                  df_prog_passes.end_x, df_prog_passes.end_y,
+                  color="k", alpha=0.2, zorder=1,
+                  headaxislength=4, headwidth=5, headlength=5,
+                  ax=axes["A"])
+vert_pitch.scatter(df_prog_passes.x, df_prog_passes.y, ax=axes["A"],
+                   s=200, color="k", alpha=0.2, zorder=1)
+labels = vert_pitch.label_heatmap(bin_statistic, color='#f4edf0', fontsize=22,
+                                  ax=axes["A"], ha='center', va='center',
+                                  str_format='{:.0%}')
+## ------- PLOT PERCENTILE RANKING (E) ----------- ##
+bars = axes["B"].barh(y=df_fbref_percentile_player.index,
                width=df_fbref_percentile_player.values,
                edgecolor="k", hatch="///",
                color = cmap(norm(df_fbref_percentile_player.values)))
-axes["E"].bar_label(bars, labels=df_fbref_player.values,
+axes["B"].bar_label(bars, labels=df_fbref_player.values,
              fontweight="bold", fontsize=14)
 
-# legend to our plot
-cbar = plt.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap), cax=axes["F"])
-
 # add vertical line
-axes["E"].axvline(0.5, 0, 1, zorder=5, lw=2, color="k", ls="--")
+axes["B"].axvline(0.5, 0, 1, zorder=5, lw=2, color="k", ls="--")
 
 # formatting
-axes["E"].set_yticks(range(len(passing_names)), passing_names, fontsize=12, fontweight="bold")
-axes["E"].set_xticks([0, 0.25, 0.5, 0.75, 1], [0, 0.25, 0.5, 0.75, 1], fontsize=12)
-axes["E"].spines.right.set_visible(False)
-axes["E"].spines.top.set_visible(False)
+axes["B"].set_yticks(range(len(passing_names)), passing_names, fontsize=12, fontweight="bold")
+axes["B"].set_xticks([0, 0.25, 0.5, 0.75, 1], [0, 0.25, 0.5, 0.75, 1], fontsize=12)
+axes["B"].spines.right.set_visible(False)
+axes["B"].spines.top.set_visible(False)
+
+# legend to our plot
+cbar = plt.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap), cax=axes["C"])
