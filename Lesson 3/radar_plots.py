b74550a6bb409fd9b4e7307ce6668c34ecddc491
Christian Rønsholt <ronsholt32@gmail.com>
2022-09-14 19:46:20 +0200

    added radar plots to lesson 3

---
+++ Lesson 3/radar_plots.py
@@ -0,0 +1,19 @@
+# https://soccermatics.readthedocs.io/en/latest/gallery/lesson3/plot_RadarPlot.html
+import pandas as pd
+import numpy as np
+import json
+# plotting
+import matplotlib.pyplot as plt
+# statistical fitting of models
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+#used for plots
+from scipy import stats
+from mplsoccer import PyPizza, FontManager
+
+from utility_functions import calulatexG, FinalThird
+
+# Load data
+with open("../data/wyscout/events/events_England.json") as f:
+    data = json.load(f)
+train = pd.DataFrame(data)
